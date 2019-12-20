d763e051a73e8e7371574f550e6f16426a22b638
luis rojas <luis_brabo@hotmail.com>
2019-12-20 01:36:42 +0100

    inicio ejs y comienzo de datos personales

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app =  express();
 const path =  require("path");
 
+//definimos el motor de vistas que vamos a usar
 app.set("view engine","ejs");
 app.set(path.join(__dirname,"views"));
 
-app.listen(config.port, function (err) {
+
+//usamos el middleware static
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.get("/prueba",function(request,response){
+    response.render("datosPersonales");
+});
+app.listen(3000, function (err) {
     if (err) {
         console.error("No se pudo inicializar el servidor: "
             + err.message);
