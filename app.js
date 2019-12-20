80bbd81f43f59a48914036db6ca046b04ecad509
luis rojas <luis_brabo@hotmail.com>
2019-12-20 20:18:02 +0100

    Datos personales acabados y comienzo de idiomas.

--- app.js
+++ app.js
@@ -9,10 +9,16 @@ app.set(path.join(__dirname,"views"));
 
 //usamos el middleware static
 app.use(express.static(path.join(__dirname,"public")));
+app.get("/inicio",function(request,response){
+    response.render("inicio");
+});
 
-
-app.get("/prueba",function(request,response){
+app.get("/datosPersonales",function(request,response){
     response.render("datosPersonales");
+});
+
+app.get("/idiomas",function(request,response){
+    response.render("idiomas");
 });
 app.listen(3000, function (err) {
     if (err) {
